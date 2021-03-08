89971d34e75604a25191acd8048e7917280c8233
Fernando <carvalho.fdec@gmail.com>
2021-03-07 22:32:00 -0300

    condicional

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+console.log('teste')
+var vel = 20
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade')
+} 
+else if (vel < 30) {
+    console.log('Você está muito lendo para a via')
+} else {
+    console.log('Siga com cuidado')
+} 
