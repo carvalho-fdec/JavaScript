1ed5d3c9a7f597fcddcd333357468149de559ab0
Fernando <carvalho.fdec@gmail.com>
2021-03-15 21:08:27 -0300

    repetição

---
+++ repeticao.js
@@ -0,0 +1,21 @@
+var c = 0
+console.log('\ninicio do while')
+do {
+    console.log(`contador ${c}`)
+    c++
+} while (c <= 3)
+console.log('fim do while')
+
+console.log('\ninicio while')
+c = 0
+while (c <= 3) {
+    console.log(`contador ${c}`)
+    c++
+}
+console.log('fim while')
+
+console.log('\ninicio for')
+for (c = 0; c <= 3; c++) {
+    console.log(`contador ${c}`)
+}
+console.log('fim for')
