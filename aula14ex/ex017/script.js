fa674cdf55d4ae7da717e0b87d836217eac045a4
Fernando <carvalho.fdec@gmail.com>
2021-03-16 20:33:03 -0300

    tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,13 @@
-alert('test')
+function tabuada () {
+    var num = Number(document.querySelector('input#num').value)
+    var res = document.getElementById('tabuada')
+    
+    res.innerHTML = ''
+    for (var cont=0; cont<=10; cont++) {
+        //res += `${num} X ${cont} = ${(num*cont)}<br>`
+        //alert(`${num} X ${cont} = ${(num*cont)}<br>`)
+        let item = document.createElement('option')
+        item.text = `${num} x ${cont} = ${num*cont}`
+        res.appendChild(item)
+    }
+}
